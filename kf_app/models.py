85d1a058f807f71ee0d25b8cda1f0379fe862c91
Студент группы IS <is@sielom.local>
2025-09-26 10:19:54 +0500

    /

--- kf_app/models.py
+++ kf_app/models.py
@@ -5,6 +5,25 @@ class Author(models.Model):
     surname = models.CharField("Фамилия", max_length=25)
     birthday = models.DateField("Дата рождения")
     bio = models.TextField("Биография")
+    dessc = models.CharField("Жив или нет", default="No")
+
+    class Meta:
+        verbose_name = "Автор"
+        verbose_name_plural = "Авторы"
+        ordering = ["surname", "name"]
+        indexes = [
+            models.Index(fields=["surname"])
+        ]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["surname", "bio"],
+                condition=models.Q(dessc = "Жив"),
+                name = "unique_surname_bio"
+            ),
+        ]
+
+        def __str__(self):
+            return f"{self.surname} {self.name}"
 
 class Publisher(models.Model):
     name = models.CharField("Название", unique=True, blank=True)
